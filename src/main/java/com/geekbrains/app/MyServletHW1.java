095e37e75f1a125c424bfe018fe3dedb9ec6ef09
EvAlGodGood <Gooodfile@mail.ru>
2021-07-22 08:01:52 +0800

    1

---
+++ src/main/java/com/geekbrains/app/MyServletHW1.java
@@ -0,0 +1,14 @@
+package com.geekbrains.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServletHW1 extends HttpServlet {
+
+}
---
+++ src/main/java/com/geekbrains/app/Product.java
@@ -0,0 +1,5 @@
+package com.geekbrains.app;
+
+public class Product {
+
+}
