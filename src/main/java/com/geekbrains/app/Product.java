608612fcc1ff0c73b5b901e48a4fafa6966e188b
EvAlGodGood <Gooodfile@mail.ru>
2021-07-22 09:00:15 +0800

    2. Добавлен класс Product и Свой сервлет выводящий при обращении в браузер таблицу из 10 продуктов/товаров

--- src/main/java/com/geekbrains/app/Product.java
+++ src/main/java/com/geekbrains/app/Product.java
@@ -1,5 +1,31 @@
 package com.geekbrains.app;
 
 public class Product {
+    String id;
+    String title;
+    String cost;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
 }
